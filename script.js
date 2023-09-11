36736def091c3f0a38a617a00a7056eb7dda9a4c
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-11 21:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+document.getElementById('ageForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the form from submitting and refreshing the page
+
+    const nameInput = document.getElementById('name').value;
+    const ageInput = parseInt(document.getElementById('age').value);
+
+    if (!nameInput || !ageInput) {
+        alert('Please fill in both name and age.');
+        return;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (ageInput >= 18) {
+                resolve(`Welcome, ${nameInput}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${nameInput}. You aren't old enough.`);
+            }
+        }, 4000); // Simulate a delay of 4 seconds
+    });
+
+    promise
+        .then((message) => {
+            alert(message);
+        })
+        .catch((error) => {
+            alert(error);
+        });
+
+    // Clear the form inputs
+    document.getElementById('name').value = '';
+    document.getElementById('age').value = '';
+});
+
